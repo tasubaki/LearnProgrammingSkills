e8a378ace9a8d0140b9f803ddb0f3acd61cc45a9
tasubaki <namdangutc1048@gmail.com>
2024-09-18 10:00:46 +0700

    Add Veriables file

---
+++ Veriables.py
@@ -0,0 +1,30 @@
+# Creating Variables
+num_integer = 8 
+num_float = 8.88888
+string_var = "Hi Code"
+
+print(num_integer)
+print(num_float)
+print(string_var)
+
+var = 8 # var is of type int
+var = "Hello " +"titi" # var is now of type str
+
+print (var) # Result: Hello titi; Auto change data type and get last value
+
+
+# Casting
+number = int(8) # number will be 3
+number1 = float(8) # number will be 3.0
+number2 = str(8) # number will be '3'
+fruits = ["apple", "banana", "cherry"]
+
+print(type(number)) # Result: <class 'int'>
+print(type(number1)) # Result: <class 'float'>
+print(type(number2)) # Result: <class 'str'>
+print(type(fruits)) # Result: <class 'list>
+
+
+x, y, z = "Orange", "Banana", 8
+
+print(z)
