df67b9678a93b4377edc2f6c4c353fc0d0266df3
tasubaki <namdangutc1048@gmail.com>
2024-09-20 17:27:45 +0700

    Update Readme file and exercises

--- Training/CheckLeapYear.py
+++ Training/CheckLeapYear.py
@@ -3,11 +3,10 @@ Viết chương trình nhập vào 1 năm dương lịch, kiểm tra năm đó c
 Đếm số ngày trong tháng.
 """
 
-
 year = int(input("Enter year: "))
 month = int(input("Enter month: "))
 
-""" # Solution 1 use if...elif
+# Solution 1 use if...elif
 if (year%4==0 and year%100!=0) or year%400==0:
     if month==1 or month==3 or month==5 or month ==7 or month==8 or month==10 or month==12:
     #if month in (1, 3, 5, 7, 8, 12):
@@ -23,7 +22,7 @@ else:
         print(f"{year} is not a leap year and {month} has 30 days!")
     else:
         print(f"{year} is not a leap year and {month} has 29 days!") 
-"""
+
 
 # Solution 2 use list
 # Check leap year
