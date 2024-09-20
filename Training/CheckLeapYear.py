b1832705b38fafb247eccf3fd1697dd8d04c1afa
tasubaki <namdangutc1048@gmail.com>
2024-09-20 14:24:59 +0700

    Update excercise if...else

--- Training/CheckLeapYear.py
+++ Training/CheckLeapYear.py
@@ -10,6 +10,7 @@ month = int(input("Enter month: "))
 """ # Solution 1 use if...elif
 if (year%4==0 and year%100!=0) or year%400==0:
     if month==1 or month==3 or month==5 or month ==7 or month==8 or month==10 or month==12:
+    #if month in (1, 3, 5, 7, 8, 12):
         print(f"{year} is a leap year and {month} has 31 days!")
     elif month==4 or month==6 or month==9 or month==11:
         print(f"{year} is a leap year and {month} has 30 days!")
